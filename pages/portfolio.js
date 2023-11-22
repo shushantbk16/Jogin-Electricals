5b69bf8749445e6282ab75c21ef7755dec681c13
shushantbk16 <119441991+shushantbk16@users.noreply.github.com>
2023-11-22 15:29:39 +0530

    first commit

---
+++ pages/portfolio.js
@@ -0,0 +1,13 @@
+import  Portfolio  from "@/sections/Portfolio";
+import Head from "next/head";
+import React from "react";
+
+const portfolio = () => {
+  return (
+    <>
+       <Portfolio />
+    </>
+  );
+};
+
+export default portfolio;
