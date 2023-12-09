8ec79963ddabd73326a40495667989b9ed9909fc
shushantbk16 <119441991+shushantbk16@users.noreply.github.com>
2023-12-09 22:09:08 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
+      <link rel="icon" href="/logo-3.png" />
         <title>Jogin Electricals</title>
       </Head>
       <Hero />
